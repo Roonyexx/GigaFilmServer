cf443e75847e9d574bce9c51daa817e8b252b02c
axemybestfriend <axemybestfriend@mail.ru>
2025-07-09 12:37:18 +0700

    Создания эмбеддингов фильмов и сериалов,их рекомендация

--- src/recommend/embeddingCreate.py
+++ src/recommend/embeddingCreate.py
@@ -4,14 +4,16 @@ import numpy as np
 from sentence_transformers import SentenceTransformer
 import torch
 
+# Подключение к БД
 conn = psycopg2.connect(
     dbname="GigaFilm",
     user="postgres",
-    password="1",
+    password="12345",
     host="localhost",
     port="5432"
 )
 
+# Модель
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model = SentenceTransformer('all-MiniLM-L6-v2', device=device)
 
@@ -20,47 +22,61 @@ def embed_text(text):
         return np.zeros(model.get_sentence_embedding_dimension())
     return model.encode(text, convert_to_numpy=True)
 
-query_films = """
-    SELECT f.id, f.overview, d.name as director_name
+query_content = """
+    SELECT f.id, f.overview, d.name as director_name, 'film' as content_type
     FROM film f
     LEFT JOIN director d ON f.director_id = d.id 
-    WHERE 
-    f.vote_count > 100
+    WHERE f.vote_count > 50
+    UNION ALL
+    SELECT t.id, t.overview, NULL as director_name, 'tv' as content_type
+    FROM tv t
+    WHERE t.vote_count > 50
 """
-films_df = pd.read_sql_query(query_films, conn)
+
+content_df = pd.read_sql_query(query_content, conn)
 
 query_genres = """
-    SELECT fg.film_id, g.name as genre_name
+    SELECT film_id as content_id, g.name as genre_name, 'film' as content_type
     FROM film_genre fg
     JOIN genre g ON fg.genre_id = g.id
+    UNION ALL
+    SELECT tv_id as content_id, g.name as genre_name, 'tv' as content_type
+    FROM tv_genre tg
+    JOIN genre g ON tg.genre_id = g.id
 """
 genres_df = pd.read_sql_query(query_genres, conn)
 
 query_actors = """
-    SELECT fa.tv_id as film_id, a.name as actor_name
+    SELECT fa.film_id as content_id, a.name as actor_name, 'film' as content_type
     FROM film_actor fa
     JOIN actor a ON fa.actor_id = a.id
+    UNION ALL
+    SELECT ta.tv_id as content_id, a.name as actor_name, 'tv' as content_type
+    FROM tv_actor ta
+    JOIN actor a ON ta.actor_id = a.id
 """
 actors_df = pd.read_sql_query(query_actors, conn)
 
-film_to_genres = genres_df.groupby('film_id')['genre_name'].apply(list).to_dict()
-film_to_actors = actors_df.groupby('film_id')['actor_name'].apply(list).to_dict()
+genre_map = genres_df.groupby(['content_id', 'content_type'])['genre_name'].apply(list).to_dict()
+actor_map = actors_df.groupby(['content_id', 'content_type'])['actor_name'].apply(list).to_dict()
 
 embeddings = []
-film_ids = []
+content_ids = []
+content_types = []
 
-for idx, row in films_df.iterrows():
-    film_id = row['id']
+for idx, row in content_df.iterrows():
+    content_id = row['id']
+    content_type = row['content_type']
     overview = row['overview'] if row['overview'] else ''
     director = row['director_name'] if row['director_name'] else ''
 
     desc_emb = embed_text(overview)
 
-    genres = film_to_genres.get(film_id, [])
+    genres = genre_map.get((content_id, content_type), [])
     genre_embs = np.array([embed_text(g) for g in genres]) if genres else np.zeros_like(desc_emb)
     genre_emb = genre_embs.mean(axis=0) if len(genres) > 0 else np.zeros_like(desc_emb)
 
-    actors = film_to_actors.get(film_id, [])
+    actors = actor_map.get((content_id, content_type), [])
     actor_embs = np.array([embed_text(a) for a in actors]) if actors else np.zeros_like(desc_emb)
     actor_emb = actor_embs.mean(axis=0) if len(actors) > 0 else np.zeros_like(desc_emb)
 
@@ -69,14 +85,16 @@ for idx, row in films_df.iterrows():
     final_emb = np.concatenate([desc_emb, genre_emb, actor_emb, director_emb])
 
     embeddings.append(final_emb)
-    film_ids.append(film_id)
+    content_ids.append(content_id)
+    content_types.append(content_type)
 
-
-    print(f"Обработано фильмов: {idx}")
+    if (idx + 1) % 1000 == 0:
+        print(f"Обработано: {idx + 1}/{len(content_df)}")
 
 embeddings = np.array(embeddings)
+np.save('content_combined_embeddings.npy', embeddings)
 
-np.save('film_combined_embeddings.npy', embeddings)
-pd.DataFrame({'id': film_ids}).to_csv('film_ids.csv', index=False)
+pd.DataFrame({'id': content_ids,'content_type': content_types
+              }).to_csv('content_ids.csv', index=False)
 
-print("Эмбеддинги созданы и сохранены.")
+print("Эмбеддинги фильмов и сериалов успешно сохранены.")
--- src/recommend/recommend.py
+++ src/recommend/recommend.py
@@ -1,36 +1,45 @@
 import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
+from sympy.strategies.core import switch
 
-embeddings = np.load("./set/film_combined_embeddings.npy")
+embeddings = np.load("content_combined_embeddings.npy")
+content_df = pd.read_csv("content_ids.csv")
 
-film_ids = pd.read_csv("film_ids.csv")["id"].tolist()
+liked = [(1396, 'tv'),(209867, 'tv'),(299536, 'tv'),(60574, 'tv'),(1416, 'tv')]
+disliked = [(271110, 'film')]
 
-liked_ids = [299536,157336,293660,671,106646,11324,150540,451048,507086]   
-disliked_ids = [254,591634,447061,672]     
+def get_indexes(ids_with_type, df):
+    idxs = []
+    for content_id, content_type in ids_with_type:
+        matched = df[(df['id'] == content_id) & (df['content_type'] == content_type)].index
+        if not matched.empty:
+            idxs.append(matched[0])
+    return idxs
 
-liked_indexes = [i for i, fid in enumerate(film_ids) if fid in liked_ids]
+liked_indexes = get_indexes(liked, content_df)
 if not liked_indexes:
-    raise ValueError("Нет валидных ID понравившихся фильмов среди загруженных")
+    raise ValueError("Нет валидных понравившихся элементов среди загруженных")
 
 liked_embeddings = embeddings[liked_indexes]
 user_profile = liked_embeddings.mean(axis=0)
 
-disliked_indexes = [i for i, fid in enumerate(film_ids) if fid in disliked_ids]
+disliked_indexes = get_indexes(disliked, content_df)
 if disliked_indexes:
     disliked_embeddings = embeddings[disliked_indexes]
     user_profile -= disliked_embeddings.mean(axis=0)
 
 similarities = cosine_similarity([user_profile], embeddings)[0]
 
-seen_ids = set(liked_ids + disliked_ids)
-recommendation_candidates = [(i, sim) for i, sim in enumerate(similarities) if film_ids[i] not in seen_ids]
+seen_ids = set(liked + disliked)
+recommendation_candidates = [(i, sim) for i, sim in enumerate(similarities)
+                             if (content_df.loc[i, 'id'], content_df.loc[i, 'content_type']) not in seen_ids]
 
 recommendation_candidates.sort(key=lambda x: x[1], reverse=True)
 top_n = 10
 top_indexes = [i for i, sim in recommendation_candidates[:top_n]]
-top_ids = [film_ids[i] for i in top_indexes]
+top_recommendations = content_df.loc[top_indexes]
 
-print("Top рекомендованные фильмы (по ID):")
-for rank, fid in enumerate(top_ids, 1):
-    print(f"{rank}. ID: https://www.themoviedb.org/movie/{fid}")
+print("Toп рекомендованные фильмы и сериалы:")
+for rank, row in enumerate(top_recommendations.itertuples(), 1):
+    print(f"{rank}. ID: {row.id}, Тип: {row.content_type}")
